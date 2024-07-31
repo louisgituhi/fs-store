271cd6816b9e5f98d2086b8e129313e3afe09e20
louis <louisgituhi@gmail.com>
2024-07-31 22:57:29 +0300

    added cards

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,19 @@ import {
   ChevronRight,
   Copy,
   CreditCard,
+  DollarSign,
+  Users,
+  Activity,
+  ArrowUpRight,
   File,
   ListFilter,
   MoreVertical,
   Truck,
 } from "lucide-react"
-
+import Link from "next/link"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import {
   Card,
   CardContent,
@@ -65,7 +70,67 @@ export default function Dashboard() {
         <DesktopNavigationLinks />
           <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
             <MobileNavigationLinks />
-            <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3"></main>
+            {/* <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3"></main> */}
+            <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+              <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+              <Card x-chunk="dashboard-01-chunk-0">
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-sm font-medium">
+                    Total Filings
+                  </CardTitle>
+                  <DollarSign className="h-4 w-4 text-muted-foreground" />
+                </CardHeader>
+                <CardContent>
+                  <div className="text-2xl font-bold">+820</div>
+                  <p className="text-xs text-muted-foreground">
+                    +20.1% from last month
+                  </p>
+                </CardContent>
+              </Card>
+
+              <Card x-chunk="dashboard-01-chunk-1">
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-sm font-medium">
+                    Personal Accounts
+                  </CardTitle>
+                  <Users className="h-4 w-4 text-muted-foreground" />
+                </CardHeader>
+                <CardContent>
+                  <div className="text-2xl font-bold">+746</div>
+                  <p className="text-xs text-muted-foreground">
+                    +180.1% from last month
+                  </p>
+                </CardContent>
+              </Card>
+
+              <Card x-chunk="dashboard-01-chunk-2">
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-sm font-medium">Business Accounts</CardTitle>
+                  <CreditCard className="h-4 w-4 text-muted-foreground" />
+                </CardHeader>
+                <CardContent>
+                  <div className="text-2xl font-bold">+74</div>
+                  <p className="text-xs text-muted-foreground">
+                    +19% from last month
+                  </p>
+                </CardContent>
+              </Card>
+
+              <Card x-chunk="dashboard-01-chunk-3">
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-sm font-medium">Retrievals Now</CardTitle>
+                  <Activity className="h-4 w-4 text-muted-foreground" />
+                </CardHeader>
+                <CardContent>
+                  <div className="text-2xl font-bold">+200</div>
+                  <p className="text-xs text-muted-foreground">
+                    +201 since last hour
+                  </p>
+                </CardContent>
+              </Card>
+              
+              </div>
+            </main>
           </div>
       </div>
     </TooltipProvider>
